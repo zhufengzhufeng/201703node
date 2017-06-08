7b2b755e8a8bf7d99ef7611a3b3cf72a7af14fa4
pamzer156 <1035465285@qq.com>
2017-06-08 15:33:55 +0800

    lists

---
+++ secondday/node.md
@@ -0,0 +1,13 @@
+## 路由 （ 前端路由 && 不刷新页面）
+通过路径的变化 显示不同的内容
+
+- hash值 #
+- 浏览器自带的 history
+
+```
+window.history.go(-1)
+window.history.back(-1)
+h5 window.history.pushState({},null,'/aaa');路径可以存在也可以不存在
+```
+
+> 如果想使用history.pushState 是需要后台支持的（页面不存在刷新会导致404）,开发时采用hash的方式，上线可以改成pushState
--- secondday/todo.js
+++ secondday/todo.js
@@ -4,6 +4,7 @@ let vm = new Vue({
         todos:[
             {isSelected:true,title:'晚上回去睡觉'}
         ],
+        hash:'complete',//路径切换时 获取的hash值
         todo:''// 输入框中需要增加的内容
     },
     methods:{
@@ -11,17 +12,35 @@ let vm = new Vue({
             let todo = {isSelected:false,title:this.todo};
             this.todos.push(todo);
             this.todo = '';
+        },
+        remove(todo){
+            this.todos = this.todos.filter(item=>todo!==item);
         }
     },
     computed:{
+        lists(){
+            if(this.hash === 'complete'){
+                return this.todos;
+            }
+            if(this.hash === 'finish'){
+                return this.todos.filter(item=>item.isSelected)
+            }
+            if(this.hash === 'unfinish'){
+                return this.todos.filter(item=>!item.isSelected)
+            }
+        },
         total(){
             //求出数组中为false的个数
             //将数组中的true全部干掉，求出剩余length
             return this.todos.filter(item=>!item.isSelected).length;
         }
-    }
-})
-
+    },
+});
+let listener = () => {
+    let hash = window.location.hash.slice(1);
+    vm.hash = hash;
+};
+window.addEventListener('hashchange',listener,false);
 //1.将数据渲染到页面上 √
 //2.获取数据将内容插入到列表中  √
 //4.算出 当前未完成的事件的个数  √

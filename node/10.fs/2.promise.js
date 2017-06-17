5e9f2c24c9f12b84bf8c05de8c57cd382a84a20a
zhufengzhufeng <894918097@qq.com>
2017-06-17 13:05:22 +0800

    writeFile

--- node/10.fs/2.promise.js
+++ node/10.fs/2.promise.js
@@ -2,20 +2,30 @@ let fs = require('fs');
 function read(filename,callback) {
     return new Promise((resolve,reject)=>{
         fs.readFile(filename,'utf8',function (err,data) {
-            if(err)reject(err);
+            if(err)reject('ok');
             resolve(data)
         });
-    })
+    });
 }
+//1. async 要和 await 连用 async 是用来修饰函数的 只有在 async中才可以 使用await
+let result =async function () {
+   let name =  await read('./a.txt'); //await紧跟着的必须是promise对象，可以像写同步方法一样来写异步
+   let age = await read('./b.txt');
+   let school = {name,age};
+   console.log(school);
+};
+result();
+
+
 //只有promise对象才有then方法
 // read('./age.txt').then((data)=>{console.log(data);},(err)=>{});
 //all方法是Promise自带的方法，参数是promise执行的列表，当所有promise全部完成，会调用then中的成功的回调，如果有一个失败 调用then中的失败的回调，会把所有的结果放到数组中，返回的结果是数组
                                                         //data=['我很帅',8]
-                                                        //[name,age] = ['我很帅',8]
-Promise.all([read('./name.txt'),read('./age1.txt')]).then(([name,age])=>{//data是数组类型按照请求的数据放入对应的结果
-    let school = {name,age}
+                                                        //[b,a] = ['我很帅',888]
+/*Promise.all([read('./name.txt'),read('./age.txt')]).then(([a,b])=>{//data是数组类型按照请求的数据放入对应的结果
+    let school = {name:a,age:b};
     console.log(school);
 },(err)=>{
     console.log(err);
-});
+});*/
 //async await es7 基于promise的
---
+++ node/10.fs/3.write.js
@@ -0,0 +1,6 @@
+let fs = require('fs');
+//1.写入的内容都是什么类型，写入是utf8格式
+//2.如果文件不存在 则创建 有内容则清空，在写入新的内容 w
+//let buffer = new Buffer('hello1');
+let obj = {name:1,age:2};//如果是对象 要写入文件需要JSON.stringify
+fs.writeFileSync('./age.txt',JSON.stringify(obj));

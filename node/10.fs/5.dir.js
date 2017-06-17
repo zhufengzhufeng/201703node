4a34fac98dfa7a804593766e19aea9214f1f0455
zhufengzhufeng <894918097@qq.com>
2017-06-17 16:21:27 +0800

    dir

--- node/10.fs/5.dir.js
+++ node/10.fs/5.dir.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 //a     0,1
 //a/b   0,2
 //a/b/c 0,3
-function makep(p) {
+/*function makep(p) {
     let dirArrs = p.split('/');
     for(let i = 0; i<dirArrs.length;i++){
         //先判断要创建的路径是否存在
@@ -15,6 +15,29 @@ function makep(p) {
             fs.mkdirSync(path);
     }
 }
-makep('g/b/c/e/f');
-fs.exists('g',function (flag) {console.log(flag);}); //递归创建
+makep('g/b/c/e/f');*/
+//fs.exists('g',function (flag) {console.log(flag);}); //递归创建
 //先创建第一层，创建这个方法 提取出来,每次创建成功后 在调用此函数，实现异步递归创建
+//插入排序
+function makep(url,callback) {
+    let dirArr = url.split('/');
+    let index = 0;//这个表示可以递增
+    function make(path) {
+        if(index > dirArr.length-1) return callback();
+        fs.exists(path,function (flag) { //判断文件是否存在
+            let p = dirArr.slice(0,++index+1).join('/');//下次要创建的路径
+            if(!flag){
+                fs.mkdir(path,function () {
+                    make(p);
+                })
+            }else{
+                make(p);
+            }
+        })
+    }
+    make(dirArr[index]);//先保证第一级目录存在
+}
+makep('g/b/c/e/f',function () {
+    
+});
+
